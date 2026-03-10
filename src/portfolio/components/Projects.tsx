2478dc751a213f82af9806844f4ffd53c5358541
James Blaskett <blazza1@hotmail.com>
2026-03-10 23:19:20 +1100

    Fixed the projects page carousel.

--- src/portfolio/components/Projects.tsx
+++ src/portfolio/components/Projects.tsx
@@ -2,6 +2,7 @@ import { ReactElement, useEffect, useRef, useState } from "react";
 import "./CSS/projects.css"
 import monster from "../images/screengrab.png"
 import battleship from "../images/battleship.png"
+import mask from "../images/MASK.png"
 import Typing from "./Typing.tsx";
 
 type Props = {turnToCheat: number;}
@@ -12,7 +13,8 @@ export default function Projects({turnToCheat}: Props){
   const [panel2, setPanel2] = useState<string>(vertical ? "projBottom" : "projRight")
   const noScroll = useRef<string>(vertical ? "" : "noScroll")
   const [, rerender] = useState<string[]>([])
-  const position = useRef<number[]>([1,2,3])
+  // 2 is visible, 1 and 3 are either side of it, 0 is hidden
+  const position = useRef<number[]>([1,2,3,0])
   const active = useRef<number>(2)
   const xTouch = useRef<number>(0);
   const yTouch = useRef<number>(0);
@@ -65,24 +67,24 @@ export default function Projects({turnToCheat}: Props){
       const topHalf = yUp < (window.innerHeight / 2)
       if ( xDiff > 0 ) {
         /* right swipe */ 
-        topHalf && change(position.current[0] -1)
-        !topHalf && change(position.current[2] -1)
+        topHalf && change(-1)
+        !topHalf && change(-2)
       } else {
         /* left swipe */
-        topHalf && change(position.current[2] -1)
-        !topHalf && change(position.current[0] -1)
+        topHalf && change(-2)
+        !topHalf && change(-1)
       }                       
     } else {
       if (vertical) return
       const leftHalf = xUp < (window.innerWidth / 2)
       if ( yDiff > 0 ) {
         /* up swipe */ 
-        leftHalf && change(position.current[0] -1)
-        !leftHalf && change(position.current[2] -1)
+        leftHalf && change(-1)
+        !leftHalf && change(-2)
       } else { 
         /* down swipe */
-        leftHalf && change(position.current[2] -1)
-        !leftHalf && change(position.current[0] -1)
+        leftHalf && change(-2)
+        !leftHalf && change(-1)
       }                                                                 
     }
     /* reset values */
@@ -98,33 +100,47 @@ export default function Projects({turnToCheat}: Props){
     noScroll.current = (window.innerHeight > window.innerWidth) ? "" : "noScroll";
   }
 
-  function change(num:number){
-    let temp:number[] = []
-    if (num == 0) temp = [3,1,2]
-    else if (num == 1) temp = [1,2,3]
-    else if (num == 2) temp = [2,3,1]
+  function change(num:number, max:number = 3){
+    max -= 1
+    let temp:number[] = position.current
+    function up(){temp.unshift(temp.pop()!)}
+    function dn(){temp.push(temp.shift()!)}
+    if (num === -1) {up(); num = temp[1]} // the "!" asserts that temp array is not empty so that temp.pop() is NOT undefined
+    else if (num === -2) {dn(); num = temp[1]}
+    else if (num - temp[1] > 0) dn()
+    else if (num - temp[1] < 0) up()
+    // else if (num === temp[0]) up()
+    // else if (num === temp[2] || num === temp[3]) dn()
     position.current = temp
     rerender([])
+    if (num !== position.current[1] && max > 0) setTimeout(() => change(num, max), 250)
   }
+  const alt1:string = "90s style top down video game."
+  const alt3:string = "Text based battleship game."
+  const alt4:string = "A human, an alien, and a dwarf standing around a table on a spaceship."
 
-  const project1:ReactElement = <img className={`project tp project${position.current[0]} ${noScroll.current}`} src={monster}></img>
+  const project1:ReactElement = <img className={`project tp project${position.current[0]} ${noScroll.current}`} alt={alt1} src={monster}></img>
   const project2:ReactElement = <div className={`project tp project${position.current[1]} ${noScroll.current}`}><Typing active={position.current[1] == 2 ? true : false}/></div>
   // const project2:ReactElement = <img className={`project tp project${position.current[1]} ${noScroll.current}`} src={monster}></img>
   const project3:ReactElement = <img className={`project tp project${position.current[2]} ${noScroll.current}`} src={battleship}></img>
+  const project4:ReactElement = <img className={`project tp project${position.current[3]} ${noScroll.current}`} src={mask}></img>
 
   const project1Link:ReactElement = <h2 className="tp">React RPG</h2>
   const project2Link:ReactElement = <h2 className="tp"><a href="https://monsterbasket.github.io/TypingGame/" target="_blank">Typing Game</a></h2>
   const project3Link:ReactElement = <h2 className="tp"><a href="https://github.com/MonsterBasket/battleship" target="_blank">Battleship</a></h2>
+  const project4Link:ReactElement = <h2 className="tp"><a href="https://bslinger.itch.io/tim-tam" target="_blank">M.A.S.K</a></h2>
 
   const project1Desc:ReactElement = <span className="tp">This was my "pièce de résistance" of my bootcamp.  A lot to be proud of despite being a bit janky and buggy.  On top of that the server is constantly out of RAM so it's pretty broken right now.  I've taken it off-line for now and I'll take a look when this folio site is finished.</span>
-  const project2Desc:ReactElement = <span className="tp">A JavaScript typing game. Choose a theme and type the words that appear on the screen before they reach the end. Words and background image come from separate APIs and are generated based on the theme you choose.</span>
+  const project2Desc:ReactElement = <span className="tp">A JavaScript typing game. Choose a theme and type the words that appear on the screen before they reach the end. Words and background image come from separate APIs and are generated based on the theme you choose.<br/><br/>Try typing the moving words right here in the browser, or click the title for the full game.</span>
   const project3Desc:ReactElement = <span className="tp">Battleship made in Ruby, you can't play it because it's almost impossible to host a pure Ruby app, but feel free to look at the code in the link provided.</span>
+  const project4Desc:ReactElement = <span className="tp">My Afternoon Snack (Kleptomaniac) - This was my first ever Game Jam project, for the recent Global Game Jam with a theme of "mask"!  Despite not having done any 3D work for quite a while, I joined a team as a 3D modeller.  I had to learn Blender on the fly, but managed to create the environments and hair/hats for characters.</span>
 
   return <section id="Projects">
         <div className={panel1}>
           {project1}
           {project2}
           {project3}
+          {project4}
         </div>
         <div className={panel2}>
           <div className={`projDesc tp desc${position.current[0]} ${noScroll.current}`}>
@@ -145,16 +161,23 @@ export default function Projects({turnToCheat}: Props){
               {project3Desc}
             </div>
           </div>          
+          <div className={`projDesc tp desc${position.current[3]} ${noScroll.current}`}>
+            <div>
+              {project4Link}
+              {project4Desc}
+            </div>
+          </div>          
         </div>
         <div className={`${panel1}Cover`}></div>
         <div className={`${panel1}Rule`}>
           <div className="projButtonBack">
-            <div className={`projButton active${position.current[1]}`} onClick={() => change(0)}></div>
-            <div className={`projButton active${position.current[0]}`} onClick={() => change(1)}></div>
-            <div className={`projButton active${position.current[2]}`} onClick={() => change(2)}></div>
+            <div className={`projButton active${position.current[2]}`} onClick={() => change(0)}></div>
+            <div className={`projButton active${position.current[1]}`} onClick={() => change(1)}></div>
+            <div className={`projButton active${position.current[0]}`} onClick={() => change(2)}></div>
+            <div className={`projButton active${position.current[3]}`} onClick={() => change(3)}></div>
           </div>
-          <div className="projUpButton" onClick={() => change(position.current[0] -1)}></div>
-          <div className="projDownButton" onClick={() => change(position.current[2] -1)}></div>
+          <div className="projUpButton" onClick={() => change(-1)}><div/></div>
+          <div className="projDownButton" onClick={() => change(-2)}><div/></div>
         </div>
   </section>
 }
